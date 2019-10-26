ea6852b05592208f551df07522d13e180732d8f3
bear2u <bear2u@naver.com>
2019-10-26 11:22:08 +0900

    update server & android

--- TodoSample/app/src/main/java/com/example/todosampleapp/model/SimpleResponse.java
+++ TodoSample/app/src/main/java/com/example/todosampleapp/model/SimpleResponse.java
@@ -2,4 +2,11 @@ package com.example.todosampleapp.model;
 
 public class SimpleResponse {
     boolean result;
+
+    @Override
+    public String toString() {
+        return "SimpleResponse{" +
+                "result=" + result +
+                '}';
+    }
 }
--- server/routes/users.js
+++ server/routes/users.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.post('/auth', function(req, res, next) {
-  const id = req.body.id;
-  const pwd = req.body.password;  
+  const id = req.body.email;
+  const pwd = req.body.pwd;  
   res
     .json({result : (id == "test@test.com" && pwd == "1234")});
 });
